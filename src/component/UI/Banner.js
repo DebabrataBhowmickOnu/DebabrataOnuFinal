cfb64dc631c48bdd0681c9ecbdf1260782fda138
Shuvoghosh7 <shuvogosh1610@gmail.com>
2025-02-12 10:17:22 +0600

    update code

--- src/component/UI/Banner.js
+++ src/component/UI/Banner.js
@@ -1,41 +1,43 @@
-import React from "react"
-import ME from '../../assets/images/me.png'
-import scroll from '../../assets/images/scroll_down.png'
+import React from "react";
+import ME from "../../assets/images/me.png";
+import scroll from "../../assets/images/scroll_down.png";
 import styles from "@/styles/Banner.module.css";
 import Image from "next/image";
 import Link from "next/link";
 const Banner = () => {
-    return (
-        <div className={styles.banner_contener}>
-            <div className={styles.banner_content}>
-                <div>
-                    <div className={styles.me_image}>
-                        <Image
-                            src={ME}
-                            width={194}
-                            height={194}
-                            alt="Picture of the logo"
-                        />
-                    </div>
-                    <div className={styles.banner_tex}>
-                        <h1>I am  a <span className={styles.span1}>Full Stack</span><br></br> Web <span>Developer</span></h1>
-                    </div>
-                </div>
-
-            </div>
-            <div className={styles.scroll_down}>
-                <Link href="#">
-                    <Image
-                        src={scroll}
-                        width={194}
-                        height={194}
-                        alt="Picture of the logo"
-                    />
-                </Link>
-            </div>
-
+  return (
+    <div className={styles.banner_contener}>
+      <h2>Hello</h2>
+      <div className={styles.banner_content}>
+        <div>
+          <div className={styles.me_image}>
+            <Image
+              src={ME}
+              width={194}
+              height={194}
+              alt="Picture of the logo"
+            />
+          </div>
+          <div className={styles.banner_tex}>
+            <h1>
+              I am a <span className={styles.span1}>Full Stack</span>
+              <br></br> Web <span>Developer</span>
+            </h1>
+          </div>
         </div>
-    )
+      </div>
+      <div className={styles.scroll_down}>
+        <Link href="#">
+          <Image
+            src={scroll}
+            width={194}
+            height={194}
+            alt="Picture of the logo"
+          />
+        </Link>
+      </div>
+    </div>
+  );
 };
 
 export default Banner;
